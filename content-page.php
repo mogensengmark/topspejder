5c5fc1a32f05177072ad009c1d76083c4415088c
Mogens Engmark <mogens.engmark@gmail.com>
2012-10-15 00:18:30 +0200

    Added topspejder theme content based upon underscore s (_S) theme.

---
+++ content-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package topspejder
+ * @since topspejder 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'topspejder' ), 'after' => '</div>' ) ); ?>
+		<?php edit_post_link( __( 'Edit', 'topspejder' ), '<span class="edit-link">', '</span>' ); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
