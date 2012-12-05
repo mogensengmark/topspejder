a6532db17c3bec9c3e70c3b28cd8642ffa8eb256
Mogens Engmark <me@rtsweb.dk>
2012-12-05 15:10:09 +0100

    Added column definitions.

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 get_header(); ?>
 
     <div id="_single" class="row">
-        <div id="primary" class="content-area">
+        <div id="primary" class="ninecol content-area">
             <div id="content" class="site-content _content-box" role="main">
 
             <?php while ( have_posts() ) : the_post(); ?>
