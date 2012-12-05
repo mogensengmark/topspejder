0497fd7cbbcfc5f92727148132740a3e6f67e79b
Mogens Engmark <me@rtsweb.dk>
2012-12-05 15:09:22 +0100

    Used index for designing div segment for unit data.

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ get_header(); ?>
                 <?php endif; ?>
             </div><!-- #content .site-content -->
         </div><!-- #primary .content-area -->
-    
+
     <?php get_sidebar(); ?>
-    
+
 <?php get_footer(); ?>
